44e02a35404684755235fede2f58e65ef07e4c3f
MD SHAKIB MONDAL <sakibmondal7@gmail.com>
2021-05-22 02:39:35 +0530

    readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Currency Converter
+This is a simple python based desktop application that helps in converting the currency of different countries in real-time with the help of some APIs.
--- main.py
+++ main.py
@@ -5,14 +5,11 @@ import requests
 import configuration as cn
 
 
-# function to get all the country names for convertion with the help of api from "fixer.io"
+# function to get all the country names for conversion with the help of api from "fixer.io"
 def get_options():
     response = requests.get("http://data.fixer.io/api/symbols?access_key={}".format(cn.access_key))
-    options = []
     loop = response.json()['symbols']
-    for a in loop:
-        options.append(loop[a] + "(" + a + ")")
-    return tuple(sorted(options))
+    return tuple(sorted([(loop[a] + "(" + a + ")") for a in loop]))
 
 
 # function to get the current exchange rate with the help of api from "fixer.io"
@@ -35,13 +32,13 @@ def hi(update_point, from_="Indian Rupee(INR)", to_="United States Dollar(USD)",
 root = Tk()
 
 # root window title and dimension
-root.title("Currency Conveter")
+root.title("Currency Converter")
 
 # adding a label to the root window
-#heading
-Label(root, text='Currency Conveter', bg='red', width=15, height=0,
+# heading
+Label(root, text='Currency Converter', bg='red', width=15, height=0,
       font=("Times", 50, "bold")).grid(row=0, column=1, columnspan=3)
-#spaces beside heading and below heading
+# spaces beside heading and below heading
 Label(root, text='', bg='pink', width=10, height=5).grid(row=0, column=0)
 Label(root, text='', bg='pink', width=10, height=5).grid(row=0, column=4)
 Label(root, text='', width=10, height=2).grid(row=1, column=4)
@@ -65,6 +62,7 @@ to_currency = ttk.Combobox(root, width=27, textvariable=clicked_to, style='Custo
 to_currency.current(158)
 to_currency.grid(row=2, column=3)
 
+# taking  the input from useer
 from_amount = Entry()
 from_amount.insert(0, 1)
 from_amount.grid(row=3, column=1)
@@ -76,6 +74,7 @@ ans.grid(row=3, column=3)
 
 Label(root, text='', height=5).grid(row=4, column=2)
 
+# adding the convert button
 convert_btn = Button(root, text="Convert", font=('', 13), width=25, bg="pink",
                      command=lambda: hi(from_=from_currency.get()[-4:-1], converting_amount=from_amount.get(),
                                         to_=to_currency.get()[-4:-1], update_point=ans)).grid(row=7,
